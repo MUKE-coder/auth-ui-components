a6e15cd4def9c0ca6006307ddd592eda2dcecee2
MUKE JOHNBAPTIST <64189841+MUKE-coder@users.noreply.github.com>
2026-01-28 03:09:38 +0300

    added seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,22 +11,37 @@ const _geistMono = Geist_Mono({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Better Auth UI By JB",
   description: "Created by JB",
+  creator: "jbwebdeveloper",
+  openGraph: {
+    siteName: "Better Auth UI Components",
+    url: "/",
+    images: [
+      {
+        url: "https://14j7oh8kso.ufs.sh/f/HLxTbDBCDLwfUNp55KfHlbxM69AvE1HPOaYKR4rqd8ygiztf",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    creator: "@Mjohnbaptist", // Twitter username
+    images: [
+      "https://14j7oh8kso.ufs.sh/f/HLxTbDBCDLwfUNp55KfHlbxM69AvE1HPOaYKR4rqd8ygiztf",
+    ],
+  },
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
+        url: "/images/favicon.ico",
+        sizes: "any",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: {
+      url: "/images/apple-touch-icon.png",
+      type: "image/png",
+      sizes: "180x180",
+    },
   },
 };
 
